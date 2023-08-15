68e8abdcb7886b7a1addca9c20d3434cb34400d1
kanav-bhanushali <71547881+kanav-bhanushali@users.noreply.github.com>
2023-08-15 12:45:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -26,6 +26,11 @@ function modelLoaded(){
     console.log('Model is Loaded by Kanav Bhanushali');
     }
 
+    function check(){
+        img= document.getElementById('captured_image'); 
+        classifier.classify (img, gotResult);
+    }
+
     function speak(){
         var Synth=window.speechSynthesis;
         var speak_data_1="The first prediction is"+Prediction_1;
@@ -33,3 +38,76 @@ function modelLoaded(){
         var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
         Synth.speak(utterThis);
     }
+
+    function gotResult(error,results)  {
+        if (error){
+            console.error(error);
+        }
+        else {
+            console.log(results);
+    
+            document.getElementById("result_gesture_name").innerHTML=results[0].label;
+            document.getElementById("result_gesture_name2").innerHTML=results[1].label;
+        Prediction_1=results[0].label;
+        Prediction_2=results[1].label;
+        speak();
+    
+        if (results[0].label=="Amazing"){
+            document.getElementById("update_emoji").innerHTML="&#128076;";
+        }
+    
+        if (results[0].label=="Ok"){
+            document.getElementById("update_emoji").innerHTML="&#128077;";
+        }
+    
+        if (results[0].label=="No"){
+            document.getElementById("update_emoji").innerHTML="&#128078;";
+        }
+
+        if (results[0].label=="Victory"){
+            document.getElementById("update_emoji").innerHTML="&#9996;";
+        }
+
+        if (results[0].label=="Yo"){
+            document.getElementById("update_emoji").innerHTML="&#129304;";
+        }
+
+        if (results[0].label=="Five"){
+            document.getElementById("update_emoji").innerHTML="&#128400;";
+        }
+
+        if (results[0].label=="Live Long"){
+            document.getElementById("update_emoji").innerHTML="&#128406;";
+        }
+
+        if (results[1].label=="Amazing"){
+            document.getElementById("update_emoji2").innerHTML="&#128076;";
+        }
+    
+        if (results[1].label=="Ok"){
+            document.getElementById("update_emoji2").innerHTML="&#128077;";
+        }
+    
+        if (results[1].label=="No"){
+            document.getElementById("update_emoji2").innerHTML="&#128078;";
+        }
+
+        if (results[1].label=="Victory"){
+            document.getElementById("update_emoji2").innerHTML="&#9996;";
+        }
+
+        if (results[1].label=="Yo"){
+            document.getElementById("update_emoji2").innerHTML="&#129304;";
+        }
+
+        if (results[1].label=="Five"){
+            document.getElementById("update_emoji2").innerHTML="&#128400;";
+        }
+
+        if (results[1].label=="Live Long"){
+            document.getElementById("update_emoji2").innerHTML="&#128406;";
+        }
+
+        }
+    
+     } 
